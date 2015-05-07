40d6a71f1026003abcaab6d25f17aa2063e107c7
P.Kernevez <pkernevez@octo.com>
2015-05-08 00:12:00 +0200

    Import initial

---
+++ src/main/java/ch/bger/demo/SimpleClass.java
@@ -0,0 +1,9 @@
+package ch.bger.demo;
+
+public class SimpleClass {
+	
+	public int sum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/ch/bger/demo/SimpleClassTest.java
@@ -0,0 +1,9 @@
+package ch.bger.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleClassTest {
+
+}
